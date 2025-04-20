3733cb066b83471ba07c09a4bd3337991239832e
tariful-islam98 <abir.tariful.islam@gmail.com>
2025-04-20 17:59:12 +0600

    Refactor browser init for multi-browser support

--- src/test/java/com/practice/base/BaseTest.java
+++ src/test/java/com/practice/base/BaseTest.java
@@ -1,6 +1,7 @@
 package com.practice.base;
 
 import com.microsoft.playwright.*;
+import com.practice.utilities.ConfigReader;
 import lombok.Getter;
 
 public class BaseTest {
@@ -12,7 +13,7 @@ public class BaseTest {
 
     public static void initializeBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = createBrowserInstance();
         context = browser.newContext(new Browser.NewContextOptions()
                 .setViewportSize(1920, 1080));
         page = context.newPage();
@@ -23,5 +24,14 @@ public class BaseTest {
         if (context != null) context.close();
         if (browser != null) browser.close();
         if (playwright != null) playwright.close();
+    }
+
+    private static Browser createBrowserInstance(){
+        String browserType = ConfigReader.getProperty("browser.name", "chromium");
+        return switch (browserType.toLowerCase()) {
+            case "firefox" -> playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            case "webkit" -> playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            default -> playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        };
     }
 }
